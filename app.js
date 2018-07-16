fb72a8fb13f37693760fd0c092b1baeebde075d0
pravin130795 <41295712+pravin130795@users.noreply.github.com>
2018-07-16 20:30:48 +0530

    update port
    update port

--- app.js
+++ app.js
@@ -2,19 +2,14 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
-let userDetails = [{username: 'Alan', version: 1.0}];
 
-app.get( '/api/userDetail', function(reqst, resp) {
-    resp.json({data: userDetails});
+app.get( '/', function(reqst, resp) {
+    let userDetails = {username: 'Alan', version: 1.0}
+    resp.render("home", {data: userDetails});
 });
 
-app.post( '/api/userDetail', function(reqst, resp) {
-    let userDetail = {username: reqst.body.username, version: reqst.body.version}
-    userDetails.push(userDetail);
-    resp.json({data: userDetails});
-});
-
-app.listen(8000, function () {
-    console.log('App listening on port 8000!');
+app.listen(8080, function () {
+    console.log('App listening on port 8080!');
 });
