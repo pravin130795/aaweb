b48450d8c5150ade0581bbc957201465577ad549
Alan Dmonte <alan.dmonte@brainvire.com>
2018-07-17 11:29:45 +0530

    regarding user POST and GET api's

--- app.js
+++ app.js
@@ -1,15 +1,50 @@
 var express = require('express');
+var mysql = require('mysql');
 var bodyParser = require('body-parser');
 var app = express();
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get( '/', function(reqst, resp) {
-    let userDetails = {username: 'Alan', version: 1.0}
-    resp.render("home", {data: userDetails});
+var connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database : 'node_mysql_connection'
 });
 
-app.listen(8080, function () {
-    console.log('App listening on port 8080!');
+app.get( '/', function(rqst, resp) {
+    var q = 'SELECT * FROM users order by created_at desc';
+    connection.query(q, function (error, results) {
+        if (error) throw error;
+        resp.render("home", {data: results});
+    })
+});
+
+/* app.get("/register", function(req, res){
+    var person = {
+        email: req.query.email
+    };
+    connection.query('INSERT INTO users SET ?', person, function(err, result) {
+        if (err) throw err;
+        res.redirect("/");
+    });
+}); */
+
+app.get("/signUp", function(req, res) {
+    res.render("register");
+})
+
+app.post("/register", function(req, res){
+    var person = {
+        email: req.body.email
+    };
+    connection.query('INSERT INTO users SET ?', person, function(err, result) {
+        if (err) throw err;
+        res.redirect("/");
+    });
+});
+
+app.listen(9090, function () {
+    console.log('App listening on port 9090!');
 });
