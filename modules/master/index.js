bd914b048381621ad62cd8bbb79241a8cbe858b9
parthraval102 <parth.raval@brainvire.com>
2018-08-06 20:59:08 +0530

    Removed Old Code

--- modules/master/index.js
+++
@@ -1,8 +0,0 @@
-let service = require('./master-service');
-
-module.exports = function (app) {
-    // To Insert Designation Details To Database
-    app.post('/master/designation', service.addDesignation);
-	app.get('/master/designation', service.getDesignation);
-
-}
