54bc940385c3efe76375e39d0eaf3dc2533ad4af
pravin130795 <pravin.rathod@brainvire.com>
2018-08-13 19:57:06 +0530

    remove loger console

--- middlewares/requestLog.js
+++ middlewares/requestLog.js
@@ -6,12 +6,12 @@
 	app.use(function (req, res, next) {
 		// User may not be logged in so pass on the request
 		var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-		logger.info(util.format('%s request from:- %s', req.url, ip));
+		//logger.info(util.format('%s request from:- %s', req.url, ip));
 
 		if (req.method.toLowerCase() === 'get') {
-			logger.info(util.format('request query:- %j', req.query));
+			//logger.info(util.format('request query:- %j', req.query));
 		} else {
-			logger.info(util.format('request body:- %j', req.body));
+			//logger.info(util.format('request body:- %j', req.body));
 		}
 		next();
 	});
--- server.js
+++ server.js
@@ -21,12 +21,12 @@ app.use('/', express.static(path.join(__dirname + '/public')));
 
 // start http server
 let server = http.createServer(app).listen(app.get('port'), function () {
-	logger.info(util.format('ALGHANIM Web App with pid: %s listening on port: %s', process.pid, app.get('port')));
-	logger.info(util.format('Environment: %s', config.get('env')));
+	//logger.info(util.format('ALGHANIM Web App with pid: %s listening on port: %s', process.pid, app.get('port')));
+	//logger.info(util.format('Environment: %s', config.get('env')));
 });
 
 process.on('uncaughtException', function (e) {
-	logger.error(util.format('uncaught exception:- ', e.stack));
+	//logger.error(util.format('uncaught exception:- ', e.stack));
 });
 
 server.timeout = 600000;
